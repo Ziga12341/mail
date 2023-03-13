8490189030aa658e2ee40451bbef0a17f43cff80
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-03-13 18:38:57 +0100

    project3: mail: mailbox: do read mail do not work jet... background color not changing jet

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -65,14 +65,117 @@ function compose_email() {
 
 
 // Add functionality to show any of three inboxes: inbox, sent, and archive
+/**
+ * Load mailbox
+ * GET /emails/<str:mailbox>
+ * Sending a GET request to /emails/<mailbox> where <mailbox> is either inbox, sent, or archive will return back to you (in JSON form) a list of all emails in that mailbox, in reverse chronological order. For example, if you send a GET request to /emails/inbox, you might get a JSON response like the below (representing two emails):
+ *
+ * [
+ *     {
+ *         "id": 100,
+ *         "sender": "foo@example.com",
+ *         "recipients": ["bar@example.com"],
+ *         "subject": "Hello!",
+ *         "body": "Hello, world!",
+ *         "timestamp": "Jan 2 2020, 12:00 AM",
+ *         "read": false,
+ *         "archived": false
+ *     },
+ *     {
+ *         "id": 95,
+ *         "sender": "baz@example.com",
+ *         "recipients": ["bar@example.com"],
+ *         "subject": "Meeting Tomorrow",
+ *         "body": "What time are we meeting?",
+ *         "timestamp": "Jan 1 2020, 12:00 AM",
+ *         "read": true,
+ *         "archived": false
+ *     }
+ * ]
+ * Notice that each email specifies its id (a unique identifier), a sender email address, an array of recipients, a string for subject, body, and timestamp, as well as two boolean values indicating whether the email has been read and whether the email has been archived.
+ *
+ * How would you get access to such values in JavaScript? Recall that in JavaScript, you can use fetch to make a web request. Therefore, the following JavaScript code
+ * @param mailbox type of mailbox inbox, sent, or archive - string
+ * @returns {Promise<void>}
+ * fetch('/emails/inbox')
+ *    .then(response => response.json())
+ *    .then(emails => {
+ *    // Print emails
+ */
 function load_mailbox(mailbox) {
-  
+  console.log(`Loading ${mailbox} mailbox...`);
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-
   // Fetch the emails
+  fetch(`/emails/${mailbox}`)
+      .then(response => response.json())
+      .then(emails => {
+        emails.forEach(email =>{
+          // emails
+          email.archived = false;
+          email.read = false;
+          const sender = email.sender;
+          const subject = email.subject;
+          const body = email.body;
+          const timestamp = email.timestamp;
+          const read = email.read;
+          const archived = email.archived;
+
+          // Print each email in console
+          console.log(email);
+
+          // Create a div element for each email
+          // Use bootstrap table to display the emails
+          // do not use inerHTML
+          // td = table data, tr = table row
+
+          // Create a row for the column names
+          const emailTable = document.createElement('table');
+          emailTable.className = 'table table-sm';
+          const emailTableHead = document.createElement('thead');
+          const emailTableBody = document.createElement('tbody');
+          const emailTableRow = document.createElement('tr');
+          if(read === true) {
+            emailTableRow.className = 'table-active';
+            }
+          const emailTableDataSender = document.createElement('td');
+          const emailTableDataSubject = document.createElement('td');
+          const emailTableDataTimestamp = document.createElement('td');
+          const emailTableDataRead = document.createElement('td');
+
+          // Create a row for the column names
+          const emailTableHeaderRow = document.createElement('tr');
+          const emailTableHeaderSender = document.createElement('th');
+          const emailTableHeaderSubject = document.createElement('th');
+          const emailTableHeaderTimestamp = document.createElement('th');
+          const emailTableHeaderRead = document.createElement('th');
+
+          // Add the sender, subject, timestamp, and read to the table
+          emailTableDataSender.innerHTML = sender;
+          emailTableDataSubject.innerHTML = subject;
+          emailTableDataTimestamp.innerHTML = timestamp;
+          emailTableDataRead.innerHTML = read;
+
+          // Append the table data to the table row
+          emailTableRow.append(emailTableDataSender);
+          emailTableRow.append(emailTableDataSubject);
+          emailTableRow.append(emailTableDataTimestamp);
+          emailTableRow.append(emailTableDataRead);
+
+          emailTableBody.append(emailTableRow);
+          emailTable.append(emailTableBody);
+
+          // Append the table head to the table
+          emailTable.append(emailTableHead);
+
+          // Append the table to the emails view
+          document.querySelector('#emails-view').append(emailTable);
+
+        });
+        });
 }
