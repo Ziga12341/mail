d2d331d8e8a70ae36b09ee5ba4742d4df8f17d0c
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-03-15 16:22:53 +0100

    project3: mail: Reply: remove comments from previous task

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,10 +1,12 @@
 /*
 Using JavaScript, HTML, and CSS, complete the implementation of your single-page-app email client inside of inbox.js (and not additional or other files; for grading purposes, we’re only going to be considering inbox.js!). You must fulfill the following requirements:
 
-Archive and Unarchive: Allow users to archive and unarchive emails that they have received.
-When viewing an Inbox email, the user should be presented with a button that lets them archive the email. When viewing an Archive email, the user should be presented with a button that lets them unarchive the email. This requirement does not apply to emails in the Sent mailbox.
-Recall that you can send a PUT request to /emails/<email_id> to mark an email as archived or unarchived.
-Once an email has been archived or unarchived, load the user’s inbox.
+Reply: Allow users to reply to an email.
+When viewing an email, the user should be presented with a “Reply” button that lets them reply to the email.
+When the user clicks the “Reply” button, they should be taken to the email composition form.
+Pre-fill the composition form with the recipient field set to whoever sent the original email.
+Pre-fill the subject line. If the original email had a subject line of foo, the new subject line should be Re: foo. (If the subject line already begins with Re: , no need to add it again.)
+Pre-fill the body of the email with a line like "On Jan 1 2020, 12:00 AM foo@example.com wrote:" followed by the original text of the email.
  */
 
 // if post request compose email then Load the user’s sent mailbox
